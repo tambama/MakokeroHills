6615d6ef26be6518f6e4ce5afa1fce57791c7391
Peniel Tambama <tambama.peniel@outlook.com>
2015-08-05 11:10:34 +0200

    added data serialization

---
+++ MakokeroApi/Controllers/EventOwnersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MakokeroData;
+
+namespace MakokeroApi.Controllers
+{
+    public class EventOwnersController : ApiController
+    {
+        private MakokeroContext db = new MakokeroContext();
+
+        // GET: api/EventOwners
+        public IQueryable<EventOwner> GetEventOwners()
+        {
+            return db.EventOwners;
+        }
+
+        // GET: api/EventOwners/5
+        [ResponseType(typeof(EventOwner))]
+        public async Task<IHttpActionResult> GetEventOwner(int id)
+        {
+            EventOwner eventOwner = await db.EventOwners.FindAsync(id);
+            if (eventOwner == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(eventOwner);
+        }
+
+        // PUT: api/EventOwners/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutEventOwner(int id, EventOwner eventOwner)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != eventOwner.EventOwnerId)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(eventOwner).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EventOwnerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/EventOwners
+        [ResponseType(typeof(EventOwner))]
+        public async Task<IHttpActionResult> PostEventOwner(EventOwner eventOwner)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.EventOwners.Add(eventOwner);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = eventOwner.EventOwnerId }, eventOwner);
+        }
+
+        // DELETE: api/EventOwners/5
+        [ResponseType(typeof(EventOwner))]
+        public async Task<IHttpActionResult> DeleteEventOwner(int id)
+        {
+            EventOwner eventOwner = await db.EventOwners.FindAsync(id);
+            if (eventOwner == null)
+            {
+                return NotFound();
+            }
+
+            db.EventOwners.Remove(eventOwner);
+            await db.SaveChangesAsync();
+
+            return Ok(eventOwner);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EventOwnerExists(int id)
+        {
+            return db.EventOwners.Count(e => e.EventOwnerId == id) > 0;
+        }
+    }
+}
--- MakokeroApi/Global.asax.cs
+++ MakokeroApi/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Newtonsoft.Json;
 
 namespace MakokeroApi
 {
@@ -18,6 +19,9 @@ namespace MakokeroApi
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.All;
         }
     }
 }
